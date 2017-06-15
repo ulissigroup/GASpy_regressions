4a6fee5d0d1c49a755363dbcad06eb6f856cacb5
Kevin Tran <ktrain9891@gmail.com>
2017-06-15 15:25:55 -0700

    Modify since we are using primarily jupyter, now

--- run_regress.sh
+++ run_regress.sh
@@ -13,6 +13,7 @@ module load python
 source activate /project/projectdirs/m2755/GASpy_conda/
 cd /global/project/projectdirs/m2755/Kevin/GASpy/GASpy_regressions/
 
+jupyter nbconvert --to python regress.ipynb
 python regress.py > regression.log
 
 # CommonAdapter (SLURM) completed writing Template
