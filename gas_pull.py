808b9dcb4dd4131fd462aa916f94bfd16ad3f896
Kevin Tran <ktrain9891@gmail.com>
2017-06-19 11:37:31 -0700

    Finish renaming coordcount methods

--- gas_pull.py
+++ gas_pull.py
@@ -88,7 +88,7 @@ class GASPullByMotifs(object):
         return np.hstack(data_tup)
 
 
-    def coordcount_ads(self):
+    def coordcount_ads_to_energy(self):
         '''
         Pull data according to the following motifs:
             coord_count     A vector of ordered integers. Each integer represents the number
