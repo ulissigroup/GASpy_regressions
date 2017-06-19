b3fe6f97911b21c328f045e9e66de8038e4ef28c
Kevin Tran <ktrain9891@gmail.com>
2017-06-19 11:08:24 -0700

    Remove archives

--- Archives/vasp_settings_to_str.py
+++
@@ -1,6 +0,0 @@
-def vasp_settings_to_str(vasp_settings):
-    vasp_settings=vasp_settings.copy()
-    for key in vasp_settings:
-        if type(vasp_settings[key]) not in [str,int,float,bool]:
-            vasp_settings[key]=str(vasp_settings[key])
-    return vasp_settings
