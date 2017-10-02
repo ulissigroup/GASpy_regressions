0a851073cff6bbedeec6d78a2ebfe707ca511822
Kevin Tran <ktrain9891@gmail.com>
2017-10-02 15:09:27 -0400

    Use hierarchical model now

--- update_model.py
+++ update_model.py
@@ -1,13 +1,14 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[ ]:
 
 # Importing
 import pdb
 import sys
 from regressor import GASpyRegressor
 import gpickle
+import gplot
 sys.path.insert(0, '../')
 from gaspy.utils import vasp_settings_to_str
 
@@ -16,33 +17,45 @@ VASP_SETTINGS = vasp_settings_to_str({'gga': 'RP',
                                       'encut': 350})
 
 
-# In[2]:
+# In[ ]:
 
+import copy
 from tpot import TPOTRegressor
+from sklearn.gaussian_process import GaussianProcessRegressor
 
 
-# In[3]:
+# In[ ]:
 
-model_name = 'TPOT'
+model_name = 'GP_around_TPOT'
 features = ['coordcount', 'ads']
+outer_features = ['neighbors_coordcounts']
 responses = ['energy']
 blocks = None
+fingerprints = {'neighborcoord': '$processed_data.fp_final.neighborcoord'}
 
 
-# In[4]:
+# In[ ]:
 
 tpot = TPOTRegressor(
-                     generations=4,
-                     population_size=16,
+                     generations=1,
+                     population_size=2,
                      verbosity=2,
                      random_state=42,
                     )
-TPOT = GASpyRegressor(features=features, responses=responses,
-                      blocks=blocks, vasp_settings=VASP_SETTINGS)
-TPOT.fit_tpot(tpot, model_name=model_name)
+gp = GaussianProcessRegressor(
+                              #kernel= 1.0*RBF(length_scale=0.05) \
+                              #       +1.0*RBF(length_scale=0.2) \
+                              #       +1.0*WhiteKernel(noise_level=0.05**2.0),
+                              #n_restarts_optimizer=2,
+                             )
+H = GASpyRegressor(features=features, responses=responses,
+                   blocks=blocks, vasp_settings=VASP_SETTINGS,
+                   fingerprints=fingerprints)
+H.fit_tpot(tpot, model_name=model_name)
+H.fit_hierarchical(gp, 'fit_sk', outer_features, model_name=model_name)
 
 
-# In[9]:
+# In[ ]:
 
-gpickle.dump(TPOT)
+gpickle.dump(H)
 
