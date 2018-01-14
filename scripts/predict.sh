52935db2e9f65a490a8b57a06f5f8c72ddc2552b
Kevin Tran <ktrain9891@gmail.com>
2018-01-14 14:47:16 -0500

    Add multithreading settings and extend prediction time

--- scripts/predict.sh
+++ scripts/predict.sh
@@ -2,12 +2,16 @@
 
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=32
-#SBATCH --time=03:00:00
+#SBATCH --time=06:00:00
 #SBATCH --partition=regular
 #SBATCH --job-name=predict
 #SBATCH --output=predict-%j.out
 #SBATCH --error=predict-%j.error
 #SBATCH --constraint=haswell
+
+export MKL_NUM_THREADS=1
+export NUMEXPR_NUM_THREADS=1
+export OMP_NUM_THREADS=1
 
 # Use a surrogate model to make predictions
 python -c "from gaspy_regress.perform import prediction; prediction()"
