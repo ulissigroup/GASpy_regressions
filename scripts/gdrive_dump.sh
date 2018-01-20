b4b47d495a9abdcfdf8b7cdc4b881d9ab3af3355
Kevin Tran <ktrain9891@gmail.com>
2018-01-20 18:23:47 -0500

    Bring gdrive dump back online and fix bug with io name change

--- scripts/gdrive_dump.sh
+++ scripts/gdrive_dump.sh
@@ -12,17 +12,17 @@
 # Dump our predictions into Google spreadsheets
 
 # CO2RR
-# gsheet="gasdb_predictions"
-# worksheet="CO2RR_T"
-# predictions_path="/global/project/projectdirs/m2755/GASpy/GASpy_regressions/pkls/CO2RR_predictions_GP_around_TPOT_FEATURES_coordcount_neighbors_coordcounts_RESPONSES_energy_BLOCKS_adsorbate.pkl"
-# comparisons_path="/global/project/projectdirs/m2755/GASpy/GASpy_regressions/pkls/HER_predictions_GP_around_TPOT_FEATURES_coordcount_neighbors_coordcounts_RESPONSES_energy_BLOCKS_adsorbate.pkl"
-# identifying_labels="MPID, Miller, Top?"
-# reporting_labels="dE [eV], Performance, Mongo ID"
-# comparison_name="HER"
-# python -c "from gaspy_regress.io import gdrive_dump; gdrive_dump('$gsheet', '$worksheet', predictions_path='$predictions_path', comparisons_path='$comparisons_path', identifying_labels='$identifying_labels', reporting_labels='$reporting_labels', comparison_name='$comparison_name')"
+gsheet="gasdb_predictions"
+worksheet="CO2RR_T"
+predictions_path="/global/project/projectdirs/m2755/GASpy/GASpy_regressions/pkls/CO2RR_predictions_GP_around_TPOT_FEATURES_coordcount_neighbors_coordcounts_RESPONSES_energy_BLOCKS_adsorbate.pkl"
+comparisons_path="/global/project/projectdirs/m2755/GASpy/GASpy_regressions/pkls/HER_predictions_GP_around_TPOT_FEATURES_coordcount_neighbors_coordcounts_RESPONSES_energy_BLOCKS_adsorbate.pkl"
+identifying_labels="MPID, Miller, Top?"
+reporting_labels="dE [eV], Performance, Mongo ID"
+comparison_name="HER"
+python -c "from gaspy_regress.gio import gdrive_dump; gdrive_dump('$gsheet', '$worksheet', predictions_path='$predictions_path', comparisons_path='$comparisons_path', identifying_labels='$identifying_labels', reporting_labels='$reporting_labels', comparison_name='$comparison_name')"
 
 # HER
 gsheet="gasdb_predictions"
 worksheet="HER_T"
 predictions_path="/global/project/projectdirs/m2755/GASpy/GASpy_regressions/pkls/HER_predictions_GP_around_TPOT_FEATURES_coordcount_neighbors_coordcounts_RESPONSES_energy_BLOCKS_adsorbate.pkl"
-python -c "from gaspy_regress.io import gdrive_dump; gdrive_dump('$gsheet', '$worksheet', predictions_path='$predictions_path')"
+python -c "from gaspy_regress.gio import gdrive_dump; gdrive_dump('$gsheet', '$worksheet', predictions_path='$predictions_path')"
