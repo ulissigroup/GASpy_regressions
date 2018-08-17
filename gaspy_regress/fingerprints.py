da02ff8839a5fc77042e326815c4886bfd9689ed
Kevin Tran <ktrain9891@gmail.com>
2018-08-17 09:38:59 -0400

    Finish the inner shell fingerprinter

--- gaspy_regress/fingerprints.py
+++ gaspy_regress/fingerprints.py
@@ -1,5 +1,5 @@
 '''
-This submodule contains various functions that turn fingerprints
+This submodule contains various classes that turn fingerprints
 (formatted in dictionaries/JSONs/documents) into numeric vectors
 so that they can be fed into regression pipelines.
 '''
@@ -15,6 +15,22 @@ from gaspy.utils import read_rc
 
 
 class InnerShellFingerprinter(object):
+    '''
+    This fingerprinter converts the "inner shell" atoms---i.e., the coordinated
+    atoms---into a Nx4 array of numbers, where N is the maximum number of
+    elements seen in any of the inner shells of the training set. Each 1x4
+    vector corresponds to one of the elements present in this inner shell. The
+    numbers in the 1x4 vectors are the element's median adsorption energy, its
+    atomic number, its Pauling electronegativity, and the number of those
+    elements that show up in the coordination. We also sort the 1x4 vectors
+    such that the first 1x4 vector that shows up is the one with the lowest
+    median adsorption energy.
+
+    We also use "dummy" vectors to address the fact that we will have a
+    variable number of features/elements present in the inner shell; reference
+    Davie et al (Kriging atomic properties with a variable number of inputs, J
+    Chem Phys 2016). The out-of-bounds feature we choose is the atomic count.
+    '''
     def __init__(self, docs):
         '''
         Arg:
@@ -24,11 +40,12 @@ class InnerShellFingerprinter(object):
         '''
         self.docs = docs
         self._calculate_dummy_fp()
+        self._calculate_max_num_species()
 
 
     def _calculate_dummy_fp(self):
         '''
-        This method establishes a "dummy" value for the `chemfp0` type of feature.
+        This method establishes a "dummy" value for the 1x4 vector of information.
         This dummy value is useful when using variable number of features;
         reference Davie et al (Kriging atomic properties with a variable number of inputs,
         J Chem Phys 2016). The out-of-bounds feature we choose is the atomic count.
@@ -36,15 +53,11 @@ class InnerShellFingerprinter(object):
         Resulting attributes:
             dummy_fp            A tuple that represents a single chemfp0 fingerprint,
                                 but has the "dummy" values
-            max_num_species     An integer for the maximum number of elements/species
-                                present in any single mpid we are looking at. This is useful
-                                for figuring out how many dummy features you need to add.
         '''
         # Prerequisite calculations
         self._get_compositions_by_mpid()
         self._get_mendeleev_data()
         self._calculate_median_adsorption_energies()
-        mpids = set(doc['mpid'] for doc in self.docs)
         elements = set(self.median_adsorption_energies.keys())
 
         # Calculate `dummy_fp`
@@ -57,9 +70,21 @@ class InnerShellFingerprinter(object):
         dummy_count = 0
         self.dummy_fp = (avg_median_energy, avg_atomic_num, avg_electroneg, dummy_count)
 
-        # Calculate `max_num_species`
-        num_species = [len(self.compositions_by_mpid[mpid]) for mpid in mpids]
-        self.max_num_species = max(num_species)
+
+    def _calculate_max_num_species(self):
+        '''
+        When populating "dummy fingerprints", we need to know how many of them to make.
+        We set this number equal to the maximum number of elements present in any one
+        alloy in the training set, and we find this number here.
+
+        Resulting attributes:
+            max_num_species     An integer for the maximum number of elements/species
+                                present in any single mpid we are looking at. This is useful
+                                for figuring out how many dummy features you need to add.
+        '''
+        mpids = set(doc['mpid'] for doc in self.docs)
+        num_species_per_mpid = [len(self.compositions_by_mpid[mpid]) for mpid in mpids]
+        self.max_num_species = max(num_species_per_mpid)
 
 
     def _get_compositions_by_mpid(self):
@@ -128,9 +153,9 @@ class InnerShellFingerprinter(object):
         '''
         This method calculates the median adsorption energies on each monometallic bulk
 
-        *Notes*:
-            This function uses only the data within the `docs` argument to calculate these values.
-            This function also assumes that you have only one adsorbate---i.e., no co-adsorption
+        *Note*:
+            This function uses only the data within the initial
+            `docs` argument (fed to __init__) to calculate these values.
 
         Resulting attribute:
             median_adsorption_energies  A dictionary whose keys are the substrate elements
@@ -180,5 +205,89 @@ class InnerShellFingerprinter(object):
         return filtered_docs, remaining_elements
 
 
-    def transform(self, docs):
-        return 'pending'
+    def fingerprint_docs(self, docs, flatten=True):
+        '''
+        Convert a list of documents into a list of numerical fingerprints.
+
+        Inputs:
+            docs    A list of dictionaries that should have the keys 'mpid' and 'coordination'.
+                    The value for 'mpid' should be in the form 'mpid-23' and the value
+                    for 'coordination' should be in the form 'Cu-Cu-Cu'.
+                    Should probably come from the `gaspy.gasdb.get_catalog_docs` function.
+            flatten A boolean indicating whether or not you want to flatten the
+                    output into a numpy vector, or keep it as a list of tuples.
+                    You should probably flatten it if you plan to use it, but
+                    you can not flatten it if you want to understand/view it better.
+        Output:
+            chem_fps    A list of tuples. The length of the list is equal to the number
+                        of elements present in the coordination, and the length of the
+                        tuples is 4. The first value in each tuple is the median
+                        adsorption energy; the second value is the atomic number;
+                        the third value is the Pauling electronegativity; and the last
+                        number is the count of that element in the coordination site.
+                        If `flatten == True`, then each fingerprint will be flattened into a
+                        1-dimensional numpy array.
+        '''
+        chem_fps = [self.fingerprint_doc(doc, flatten=flatten) for doc in docs]
+        return chem_fps
+
+
+    def fingerprint_doc(self, doc, flatten=True):
+        '''
+        Convert a document into a numerical fingerprint.
+
+        Inputs:
+            doc     A dictionary that should have the keys 'mpid' and 'coordination'.
+                    The value for 'mpid' should be in the form 'mpid-23' and the value
+                    for 'coordination' should be in the form 'Cu-Cu-Cu'.
+                    Should probably come from the `gaspy.gasdb.get_catalog_docs` function.
+            flatten A boolean indicating whether or not you want to flatten the
+                    output into a numpy vector, or keep it as a list of tuples.
+                    You should probably flatten it if you plan to use it, but
+                    you can not flatten it if you want to understand/view it better.
+        Output:
+            chem_fp A list of tuples. The length of the list is equal to the number
+                    of elements present in the coordination, and the length of the
+                    tuples is 4. The first value in each tuple is the median
+                    adsorption energy; the second value is the atomic number;
+                    the third value is the Pauling electronegativity; and the last
+                    number is the count of that element in the coordination site.
+                    If `flatten == True`, then this will be flattened into a
+                    1-dimensional numpy array.
+        '''
+        chem_fp = []
+        binding_atoms = doc['coordination'].split('-')
+
+        # Sometimes there is no coordination. If this happens,
+        # then hackily fix it
+        if binding_atoms == ['']:
+            binding_atoms = set()
+
+        # Add and sort the elemental information for each element present
+        for element in set(binding_atoms):
+            try:
+                element_data = self.mendeleev_data[element]
+                energy = self.median_adsorption_energies[element]
+                atomic_number = element_data.atomic_number
+                electronegativity = element_data.electronegativity(scale='pauling')
+                count = binding_atoms.count(element)
+                chem_fp.append((energy, atomic_number, electronegativity, count))
+
+            # Tell the user if they tried to predict outside of their test space
+            except KeyError as error:
+                import sys
+                extra_message = ' because probably tried to fingerprint an element that was not in the training set'
+                raise type(error)(str(error) + extra_message).with_traceback(sys.exc_info()[2])
+
+        chem_fp = sorted(chem_fp)
+
+        # Fill in the dummy fingerprints
+        for _ in range(len(chem_fp), self.max_num_species):
+            chem_fp.append(self.dummy_fp)
+
+        # Format and flatten the data (if needed)
+        chem_fp = np.array(chem_fp)
+        if flatten:
+            chem_fp = chem_fp.flatten()
+
+        return chem_fp
--- gaspy_regress/tests/fingerprints_test.py
+++ gaspy_regress/tests/fingerprints_test.py
@@ -18,7 +18,9 @@ import os
 import pytest
 import pickle
 import mendeleev
-from gaspy.gasdb import get_adsorption_docs
+from pymatgen.ext.matproj import MPRester
+from gaspy.gasdb import get_adsorption_docs, get_catalog_docs
+from gaspy.utils import read_rc
 
 REGRESSION_BASELINES_LOCATION = '/home/GASpy/GASpy_regressions/gaspy_regress/tests/regression_baselines/fingerprints/'
 
@@ -38,33 +40,76 @@ def inner_shell_fingerprinter(request):
 
 class TestInnerShellFingerprinter(object):
     @pytest.mark.baseline
-    def test_to_create_dummy_chemfp0(self, inner_shell_fingerprinter):
-        output = (inner_shell_fingerprinter.dummy_fp, inner_shell_fingerprinter.max_num_species)
-        with open(REGRESSION_BASELINES_LOCATION + 'dummy_chemfp0.pkl', 'wb') as file_handle:
-            pickle.dump(output, file_handle)
+    def test_to_create_dummy_fp(self, inner_shell_fingerprinter):
+        with open(REGRESSION_BASELINES_LOCATION + 'dummy_fp_inner_shell.pkl', 'wb') as file_handle:
+            pickle.dump(inner_shell_fingerprinter.dummy_fp, file_handle)
 
 
-    def test__calculate_dummpy_chemfp0(self, inner_shell_fingerprinter):
-        '''
-        This test really should be parametrized, and I don't think it
-        needs to be a regression test. But I'm doing it this way because
-        I hove not made the time to do it correctly.
-        '''
-        with open(REGRESSION_BASELINES_LOCATION + 'dummy_chemfp0.pkl', 'rb') as file_handle:
-            expected_dummy_fp, expected_max_num_species = pickle.load(file_handle)
+    def test__calculate_dummy_fp(self, inner_shell_fingerprinter):
+        with open(REGRESSION_BASELINES_LOCATION + 'dummy_fp_inner_shell.pkl', 'rb') as file_handle:
+            expected_dummy_fp = pickle.load(file_handle)
         assert inner_shell_fingerprinter.dummy_fp == expected_dummy_fp
+
+
+    @pytest.mark.baseline
+    def test_to_create_max_num_species(self, inner_shell_fingerprinter):
+        with open(REGRESSION_BASELINES_LOCATION + 'max_species_inner_shell.pkl', 'wb') as file_handle:
+            pickle.dump(inner_shell_fingerprinter.max_num_species, file_handle)
+
+
+    def test__calculate_max_num_species(self, inner_shell_fingerprinter):
+        with open(REGRESSION_BASELINES_LOCATION + 'max_species_inner_shell.pkl', 'rb') as file_handle:
+            expected_max_num_species = pickle.load(file_handle)
         assert inner_shell_fingerprinter.max_num_species == expected_max_num_species
+
+
+    def test__get_compositions_by_mpid(self, inner_shell_fingerprinter):
+        '''
+        3-part test:
+            Test that the method saved the cache
+            Test that all the MPIDs we need are there
+            Test that all the data in the cache is correct
+        '''
+        # Make sure that we saved the object correctly
+        with open('/home/GASpy/GASpy_regressions/cache/mp_comp_data.pkl', 'rb') as file_handle:
+            saved_compositions_by_mpid = pickle.load(file_handle)
+        compositions_by_mpid = inner_shell_fingerprinter.compositions_by_mpid
+        assert compositions_by_mpid == saved_compositions_by_mpid
+
+        # Make sure that all of the required MPIDs are in the new object
+        required_mpids = set(doc['mpid'] for doc in inner_shell_fingerprinter.docs)
+        known_mpids = set(inner_shell_fingerprinter.compositions_by_mpid.keys())
+        assert required_mpids.issubset(known_mpids)
+
+        # Make sure that the compositions we do have are correct
+        with MPRester(read_rc('matproj_api_key')) as rester:
+            for mpid in known_mpids:
+                entry = rester.get_entry_by_material_id({'task_ids': mpid})
+                expected_composition = list(entry.as_dict()['composition'].keys())
+                assert compositions_by_mpid[mpid] == expected_composition
+
+
+    def test__get_mendeleev_data(self, inner_shell_fingerprinter):
+        mendeleev_data = inner_shell_fingerprinter.mendeleev_data
+        compositions_by_mpid = inner_shell_fingerprinter.compositions_by_mpid
+        docs = inner_shell_fingerprinter.docs
+
+        for doc in docs:
+            mpid = doc['mpid']
+            composition = compositions_by_mpid[mpid]
+            for element in composition:
+                assert mendeleev_data[element] == getattr(mendeleev, element)
 
 
     @pytest.mark.baseline
     def test_to_create_median_adsorption_energies(self, inner_shell_fingerprinter):
-        with open(REGRESSION_BASELINES_LOCATION + 'median_adsorption_energies.pkl', 'wb') as file_handle:
+        with open(REGRESSION_BASELINES_LOCATION + 'median_adsorption_energies_inner_shell.pkl', 'wb') as file_handle:
             pickle.dump(inner_shell_fingerprinter.median_adsorption_energies, file_handle)
         assert True
 
 
     def test__calculate_median_adsorption_energies(self, inner_shell_fingerprinter):
-        with open(REGRESSION_BASELINES_LOCATION + 'median_adsorption_energies.pkl', 'rb') as file_handle:
+        with open(REGRESSION_BASELINES_LOCATION + 'median_adsorption_energies_inner_shell.pkl', 'rb') as file_handle:
             expected_median_adsorption_energies = pickle.load(file_handle)
         median_adsorption_energies = inner_shell_fingerprinter.median_adsorption_energies
         assert median_adsorption_energies == expected_median_adsorption_energies
@@ -87,26 +132,27 @@ class TestInnerShellFingerprinter(object):
         assert elements == monometallic_elements
 
 
-    def test__get_compositions_by_mpid(self, inner_shell_fingerprinter):
-        # Make sure that we saved the object correctly
-        with open('/home/GASpy/GASpy_regressions/cache/mp_comp_data.pkl', 'rb') as file_handle:
-            saved_compositions_by_mpid = pickle.load(file_handle)
-        compositions_by_mpid = inner_shell_fingerprinter.compositions_by_mpid
-        assert compositions_by_mpid == saved_compositions_by_mpid
+    @pytest.mark.baseline
+    def test_to_create_docs_fingerprints(self, inner_shell_fingerprinter):
+        '''
+        The arguments should really be parametrized, but pytest can't handle
+        both fixtures and parametrization simultaneously. So we'll settle
+        for a single test case for now.
+        '''
+        docs = get_catalog_docs()
+        flatten = True
+        fingerprints = inner_shell_fingerprinter.fingerprint_docs(docs, flatten=flatten)
 
-        # Make sure that all of the required MPIDs are in the new object
-        required_mpids = set(doc['mpid'] for doc in inner_shell_fingerprinter.docs)
-        known_mpids = set(inner_shell_fingerprinter.compositions_by_mpid.keys())
-        assert required_mpids.issubset(known_mpids)
+        with open(REGRESSION_BASELINES_LOCATION + 'fingerprints_inner_shell.pkl', 'wb') as file_handle:
+            pickle.dump(fingerprints, file_handle)
+        assert True
 
 
-    def test__get_mendeleev_data_for_docs(self, inner_shell_fingerprinter):
-        mendeleev_data = inner_shell_fingerprinter.mendeleev_data
-        compositions_by_mpid = inner_shell_fingerprinter.compositions_by_mpid
-        docs = inner_shell_fingerprinter.docs
+    def test_to_fingerprint_docs(self, inner_shell_fingerprinter):
+        docs = get_catalog_docs()
+        flatten = True
+        fingerprints = inner_shell_fingerprinter.fingerprint_docs(docs, flatten=flatten)
 
-        for doc in docs:
-            mpid = doc['mpid']
-            composition = compositions_by_mpid[mpid]
-            for element in composition:
-                assert mendeleev_data[element] == getattr(mendeleev, element)
+        with open(REGRESSION_BASELINES_LOCATION + 'fingerprints_inner_shell.pkl', 'rb') as file_handle:
+            expected_fingerprints = pickle.load(file_handle)
+        assert fingerprints == expected_fingerprints
