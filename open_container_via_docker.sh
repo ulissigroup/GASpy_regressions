3c7f64df7bfc2a3be9cd53d5b5584e7718840340
Kevin Tran <ktrain9891@gmail.com>
2018-12-05 10:53:12 -0500

    Add .ssh folder to volume

--- open_container_via_docker.sh
+++ open_container_via_docker.sh
@@ -23,11 +23,13 @@ if [ $jupyter = "jupyter" ]; then
     docker run -it --rm -w "/home/jovyan/GASpy" \
         -p 8888:8888 \
         -v $gaspy_mounting_config \
+        -v $HOME/.ssh:/home/.ssh \
         ulissigroup/gaspy_regressions:latest \
         jupyter
 else
     docker run -it --rm -w "/home/jovyan/GASpy" \
         -v $gaspy_mounting_config \
+        -v $HOME/.ssh:/home/.ssh \
         ulissigroup/gaspy_regressions:latest \
         /bin/bash
 fi
