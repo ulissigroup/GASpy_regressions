a719338514d175f398186f1b68d434f92f932d02
Aini Palizhati <apalizha@andrew.cmu.edu>
2019-07-16 12:06:34 -0700

    adding scripts that create/update 2D plots

---
+++ examples/update_2D_plots.sh
@@ -0,0 +1,5 @@
+#!/bin/sh -l
+
+shifter --image=ulissigroup/gaspy_regressions:latest \
+    --volume=/global/project/projectdirs/m2755/GASpy_workspaces/GASpy:/home/jovyan/GASpy \
+        python /global/project/projectdirs/m2755/GASpy_workspaces/GASpy/GASpy_regressions/examples/update_2D_plots.py
