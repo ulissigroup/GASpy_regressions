3fe2a58495c4ff168194084b300605a3d3752ea3
Kevin Tran <ktrain9891@gmail.com>
2019-11-12 11:57:09 -0500

    Begin regressions on C and stop on OOH

---
+++ examples/regression_scripts/predict_C.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+#SBATCH --constraint=knl
+#SBATCH --nodes=1
+#SBATCH --exclusive
+#SBATCH --time=30:00:00
+#SBATCH --account=m2755
+#SBATCH --qos=regular
+#SBATCH --image=ulissigroup/gaspy_regressions:latest
+#SBATCH --volume=/global/project/projectdirs/m2755/GASpy_workspaces/GASpy:/home/GASpy
+#SBATCH --job-name=predict_C
+#SBATCH --chdir=/global/project/projectdirs/m2755/GASpy_workspaces/GASpy/logs/regressions
+#SBATCH --output=predict_C.log
+#SBATCH --error=predict_C.log
+#SBATCH --open-mode=append
+#SBATCH --mail-user=ktran@andrew.cmu.edu
+#SBATCH --mail-type=FAIL
+
+export MKL_NUM_THREADS=1
+export NUMEXPR_NUM_THREADS=1
+export OMP_NUM_THREADS=1
+
+srun shifter python -c "from gaspy_regress import cache_predictions; cache_predictions('C', processes=64)"
---
+++ examples/regression_scripts/regress_C.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+#SBATCH --constraint=haswell
+#SBATCH --nodes=1
+#SBATCH --exclusive
+#SBATCH --time=01:00:00
+#SBATCH --account=m2755
+#SBATCH --qos=regular
+#SBATCH --image=ulissigroup/gaspy_regressions:latest
+#SBATCH --volume=/global/project/projectdirs/m2755/GASpy_workspaces/GASpy:/home/GASpy
+#SBATCH --job-name=regress_C
+#SBATCH --chdir=/global/project/projectdirs/m2755/GASpy_workspaces/GASpy/logs/regressions
+#SBATCH --output=regress_C.log
+#SBATCH --error=regress_C.log
+#SBATCH --open-mode=append
+#SBATCH --mail-user=ktran@andrew.cmu.edu
+#SBATCH --mail-type=FAIL
+
+# OMP has some bug that needs this flag, now
+export KMP_INIT_AT_FORK=FALSE
+
+srun shifter python -c "from gaspy_regress import fit_model0_adsorption_energies; fit_model0_adsorption_energies('C')"
